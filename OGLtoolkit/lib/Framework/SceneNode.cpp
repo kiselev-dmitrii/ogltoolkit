6425f155ecd18e430bb13a1d2ba65d5a332b9e11
Dmitrii Kiselev <sol.linux@gmail.com>
2013-03-07 22:40:20 +0400

    Fixed SceneNode

--- OGLtoolkit/lib/Framework/SceneNode.cpp
+++ OGLtoolkit/lib/Framework/SceneNode.cpp
@@ -11,12 +11,11 @@ void SceneNode::init() {
         m_positionInWorld = vec3(0);
         m_scaleInWorld = vec3(1);
 
-        m_isUpdateWorldValues = true;
-        m_isUpdateLocalToWorldMatrix = true;
+        notifyChilds();
 }
 
-void SceneNode::updateWorldValues() {
-        if(m_isUpdateWorldValues) {
+void SceneNode::updateWorldCoords() {
+        if(m_isUpdateWorldCoords) {
                 if (m_parentNode) {
                         vec3 parentWorldPos = ((SceneNode*) m_parentNode)->positionInWorld();
                         quat parentWorldOrient = ((SceneNode*) m_parentNode)->orientationInWorld();
@@ -25,24 +24,49 @@ void SceneNode::updateWorldValues() {
                         m_positionInWorld = parentWorldOrient * (parentWorldScale * m_position) + parentWorldPos;
                         m_orientationInWorld = parentWorldOrient * m_orientation;
                         m_scaleInWorld = parentWorldScale * m_scale;
-
                 } else {
                         m_positionInWorld = m_position;
                         m_orientationInWorld = m_orientation;
                         m_scaleInWorld = m_scale;
                 }
 
-                m_isUpdateLocalToWorldMatrix = true;
-                m_isUpdateWorldValues = false;
+                m_isUpdateWorldCoords = false;
         }
-
 }
 
-void SceneNode::notifyNeedToUpdateWorldValues() {
-        m_isUpdateWorldValues = true;
+void SceneNode::notifyChilds() {
+        m_isUpdateWorldCoords = true;
+        m_isUpdateLocalToWorld = true;
+        m_isUpdateWorldToLocal = true;
+
         NodeMap::iterator it;
         for(it=m_childNodes.begin(); it!=m_childNodes.end(); ++it) {
                 ((SceneNode* )it->second)->notifyNeedToUpdateWorldValues();
+        }
+}
+
+void SceneNode::updateLocalToWorldMatrix() {
+        if(m_isUpdateLocalToWorld) {
+                updateWorldCoords();
+
+                mat3 rot = toMat3(m_orientationInWorld);
+
+                m_localToWorldMatrix = mat4(
+                        m_scaleInWorld.x*rot[0][0], m_scaleInWorld.y*rot[0][1], m_scaleInWorld.z*rot[0][2], 0,
+                        m_scaleInWorld.x*rot[1][0], m_scaleInWorld.y*rot[1][1], m_scaleInWorld.z*rot[1][2], 0,
+                        m_scaleInWorld.x*rot[2][0], m_scaleInWorld.y*rot[2][1], m_scaleInWorld.z*rot[2][2], 0,
+                        m_positionInWorld.x       , m_positionInWorld.y       , m_positionInWorld.z       , 1 );
+
+                m_isUpdateLocalToWorld = false;
+        }
+}
+
+void SceneNode::updateWorldToLocalMatrix() {
+        if(m_isUpdateWorldToLocal) {
+                updateLocalToWorldMatrix();
+                m_worldToLocalMatrix = inverse(m_localToWorldMatrix);
+
+                m_isUpdateWorldToLocal = false;
         }
 }
 
@@ -55,42 +79,41 @@ SceneNode::SceneNode(const string &name) : TreeNode(name) {
 }
 
 SceneNode::~SceneNode() {
-        TODO;
 }
 
 vec3 SceneNode::convertWorldToLocal(const vec3 &worldVec) {
-        updateWorldValues();
+        updateWorldCoords();
         return inverse(m_orientationInWorld) * (worldVec - m_positionInWorld) / m_scaleInWorld;
 }
 
 quat SceneNode::convertWorldToLocal(const quat &worldQuat) {
-        updateWorldValues();
+        updateWorldCoords();
         return inverse(m_orientationInWorld) * worldQuat;
 }
 
 vec3 SceneNode::convertLocalToWorld(const vec3 &localVec) {
-        updateWorldValues();
+        updateWorldCoords();
         return (m_orientationInWorld * (localVec * m_scaleInWorld)) + m_positionInWorld;
 }
 
 quat SceneNode::convertLocalToWorld(const quat &localQuat) {
-        updateWorldValues();
+        updateWorldCoords();
         return m_orientationInWorld * localQuat;
 }
 
 void SceneNode::setPositionInParent(const vec3 &position) {
         m_position = position;
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
 void SceneNode::setOrientationInParent(const quat &orientation) {
         m_orientation = orientation;
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
 void SceneNode::setScaleInParent(const vec3 &scale) {
         m_scale = scale;
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
 const vec3& SceneNode::positionInParent() {
@@ -116,56 +139,59 @@ void SceneNode::setOrientationInWorld(const quat &orientation) {
 }
 
 void SceneNode::setScaleInWorld(const vec3 &scale) {
-        TODO;
+        vec3 scaleInParent = ((SceneNode*)m_scaleNode)->convertWorldToLocal(scale);
+        setScaleInParent(scaleInParent);
 }
 
 const vec3& SceneNode::positionInWorld() {
-        updateWorldValues();
+        updateWorldCoords();
         return m_positionInWorld;
 }
 
 const quat& SceneNode::orientationInWorld() {
-        updateWorldValues();
+        updateWorldCoords();
         return m_orientationInWorld;
 }
 
 const vec3& SceneNode::scaleInWorld() {
-        updateWorldValues();
+        updateWorldCoords();
         return m_scaleInWorld;
 }
 
 void SceneNode::translateInLocal(const vec3 &delta) {
         m_position += m_orientation * delta;
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
 void SceneNode::translateInParent(const vec3 &delta) {
         m_position += delta;
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
 void SceneNode::translateInWorld(const vec3 &delta) {
         if (m_parentNode) {
+                ((SceneNode*)m_parentNode)->updateWorldCoords();
                 m_position += (inverse(((SceneNode*)m_parentNode)->m_orientationInWorld) * delta) / ((SceneNode*)m_parentNode)->m_scaleInWorld;
         } else {
                 m_position += delta;
         }
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
 void SceneNode::rotateInLocal(const quat &quaternion) {
         m_orientation = m_orientation * normalize(quaternion);
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
 void SceneNode::rotateInParent(const quat &quaternion) {
         m_orientation = normalize(quaternion) * m_orientation;
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
 void SceneNode::rotateInWorld(const quat &quaternion) {
+        updateWorldCoords();
         m_orientation = m_orientation * inverse(m_orientationInWorld) * normalize(quaternion) * m_orientationInWorld;
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
 void SceneNode::rotateInLocal(const vec3 &axis, float angle) {
@@ -193,41 +219,24 @@ void SceneNode::lookAt(const vec3 &target, const vec3 &up) {
                            xAxis.z, yAxis.z, zAxis.z);
         m_orientation = toQuat(matrix);
 
-        notifyNeedToUpdateWorldValues();
+        notifyChilds();
 }
 
-void SceneNode::updateLocalToWorldMatrix() {
-        mat3 rot = toMat3(m_orientationInWorld);
-        m_localToWorldMatrix = mat4(
-                m_scaleInWorld.x*rot[0][0], m_scaleInWorld.y*rot[0][1], m_scaleInWorld.z*rot[0][2], m_positionInWorld.x,
-                m_scaleInWorld.x*rot[1][0], m_scaleInWorld.y*rot[1][1], m_scaleInWorld.z*rot[1][2], m_positionInWorld.y,
-                m_scaleInWorld.x*rot[2][0], m_scaleInWorld.y*rot[2][1], m_scaleInWorld.z*rot[2][2], m_positionInWorld.z,
-                                        0 ,                         0 ,                         0 ,                   1
-                );
-}
 
 const mat4& SceneNode::localToWorldMatrix() {
-        if(m_isUpdateLocalToWorldMatrix) {
-                updateLocalToWorldMatrix();
-                m_isUpdateLocalToWorldMatrix = false;
-        }
-
+        updateLocalToWorldMatrix();
         return m_localToWorldMatrix;
 }
 
 const mat4& SceneNode::worldToLocalMatrix() {
-        if(m_isUpdateLocalToWorldMatrix) {
-                updateLocalToWorldMatrix();
-                m_worldToLocalMatrix = inverse(m_localToWorldMatrix);
-                m_isUpdateLocalToWorldMatrix = false;
-        }
+        updateWorldToLocalMatrix();
         return m_worldToLocalMatrix;
 }
 
 void SceneNode::onAddChild() {
-        m_isUpdateWorldValues = true;
+        notifyChilds();
 }
 
 void SceneNode::onRemoveChild() {
-        m_isUpdateWorldValues = true;
+        notifyChilds();
 }
